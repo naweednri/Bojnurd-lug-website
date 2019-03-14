ae3e7303986bf1f5a74887c256742997d962dabc
mehrshad firouzian <mrfirouz007@gmail.com>
2019-03-14 20:38:45 +0330

    make some changes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PostImage(models.Model):
+    title = models.CharField(max_length=15)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    date = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    images = models.ManyToManyField(PostImage)
+
+    def __str__(self):
+        return self.title
